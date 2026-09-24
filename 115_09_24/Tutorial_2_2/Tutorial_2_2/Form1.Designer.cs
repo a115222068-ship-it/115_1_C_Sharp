dbbd31a0bbda5bbc36b9c453df233659772e5378
a115222068-ship-it <a115222068@mail.shu.edu.tw>
2026-09-24 18:01:54 +0800

    2026.09.24

--- 115_09_24/Tutorial_2_2/Tutorial_2_2/Form1.Designer.cs
+++ 115_09_24/Tutorial_2_2/Tutorial_2_2/Form1.Designer.cs
@@ -28,13 +28,35 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.messageButtom = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // messageButtom
+            // 
+            this.messageButtom.Font = new System.Drawing.Font("新細明體", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.messageButtom.Location = new System.Drawing.Point(374, 154);
+            this.messageButtom.Name = "messageButtom";
+            this.messageButtom.Size = new System.Drawing.Size(304, 158);
+            this.messageButtom.TabIndex = 0;
+            this.messageButtom.Text = "顯示訊息";
+            this.messageButtom.UseVisualStyleBackColor = true;
+            this.messageButtom.Click += new System.EventHandler(this.messageButtom_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1015, 450);
+            this.Controls.Add(this.messageButtom);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button messageButtom;
     }
 }
 
